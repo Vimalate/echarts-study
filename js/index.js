f384101c5b30c1f59a1a5a52b458c2b76455d0dc
“Vimalate <1902644369@qq.com>
2020-04-13 19:09:54 +0800

    line

--- js/index.js
+++ js/index.js
@@ -72,50 +72,60 @@
 	window.addEventListener('resize', function () {
 		myCharts.resize()
 	})
-})(),
+})();
 
 // 技能列表柱状图
 (function () {
-	// 实例化对象
-	let myCharts = echarts.init(document.querySelector('.bar2 .chart'))
-	// 声明颜色数组
+	// 基于准备好的dom，初始化echarts实例
+	var myChart = echarts.init(document.querySelector(".bar1 .chart"));
+
+	var data = [70, 34, 60, 78, 69];
+	var titlename = ["HTML5", "CSS3", "javascript", "VUE", "NODE"];
+	var valdata = [702, 350, 610, 793, 664];
 	var myColor = ["#1089E7", "#F57474", "#56D0E3", "#F8B448", "#8B78F6"];
-	// 指定配置项
 	let option = {
+		//图标位置
 		grid: {
 			top: "10%",
-			left: "25%",
-			bottom: "10%",
-			// containLabel: true
+			left: "22%",
+			bottom: "10%"
 		},
 		xAxis: {
 			show: false
 		},
 		yAxis: [{
-				type: 'category',
-				data: ['印尼', '美国', '印度', '中国', '世界人口(万)'],
+				show: true,
+				data: titlename,
+				inverse: true,
 				axisLine: {
-					show: false,
+					show: false
+				},
+				splitLine: {
+					show: false
 				},
 				axisTick: {
 					show: false
 				},
-				// y轴颜色
 				axisLabel: {
-					color: "#fff"
-				},
+					color: "#fff",
+
+					rich: {
+						lg: {
+							backgroundColor: "#339911",
+							color: "#fff",
+							borderRadius: 15,
+							// padding: 5,
+							align: "center",
+							width: 15,
+							height: 15
+						}
+					}
+				}
 			},
 			{
 				show: true,
-				data: [702, 350, 610, 793, 664],
-				// 不显示y轴的线
-				axisLine: {
-					show: false
-				},
-				// 不显示刻度
-				axisTick: {
-					show: false
-				},
+				inverse: true,
+				data: valdata,
 				axisLabel: {
 					textStyle: {
 						fontSize: 12,
@@ -125,51 +135,128 @@
 			}
 		],
 		series: [{
-				name: '条',
-				type: 'bar',
-				data: [18203, 23489, 29034, 104970, 131744],
-				// 柱子之间的距离
+				name: "条",
+				type: "bar",
+				yAxisIndex: 0,
+				data: data,
 				barCategoryGap: 50,
-				//柱子的宽度
 				barWidth: 10,
-				// 柱子设为圆角
 				itemStyle: {
 					normal: {
 						barBorderRadius: 20,
-						// 修改每根柱子颜色
 						color: function (params) {
-							return myColor[params.dataIndex];
+							var num = myColor.length;
+							return myColor[params.dataIndex % num];
 						}
 					}
 				},
-				// 显示柱子内文字
 				label: {
-					show: true,
-					// 图形内显示
-					position: "inside",
-					// 文字的显示格式
-					formatter: "{c}%"
+					normal: {
+						show: true,
+						position: "inside",
+						formatter: "{c}%"
+					}
 				}
 			},
 			{
 				name: "框",
 				type: "bar",
+				yAxisIndex: 1,
 				barCategoryGap: 50,
+				data: [100, 100, 100, 100, 100],
 				barWidth: 15,
 				itemStyle: {
-					color: "none",
-					borderColor: "#00c1de",
-					borderWidth: 3,
-					barBorderRadius: 15
-				},
-				data: [19325, 23438, 31000, 121594, 134141, 681807]
+					normal: {
+						color: "none",
+						borderColor: "#00c1de",
+						borderWidth: 3,
+						barBorderRadius: 15
+					}
+				}
 			}
 		]
 	};
-	// 把配置项给实例对象
-	myCharts.setOption(option)
-	// 图表resize 自适应
-	window.addEventListener('resize', function () {
-		myCharts.resize()
-	})
+
+	// 使用刚指定的配置项和数据显示图表。
+	myChart.setOption(option);
+	window.addEventListener("resize", function () {
+		myChart.resize();
+	});
+})();
+
+// 折线图
+(function () {
+	var myChart = echarts.init(document.querySelector(".line .chart"));
+
+	let option = {
+		color: ['#00f2f1', '#ed3f35'],
+		tooltip: {
+			trigger: 'axis'
+		},
+		legend: {
+			// 如果series 有name ，则可以不用写 data
+			// data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎'],
+			textStyle: {
+				color: '#4c9bfd' // 图例文字颜色
+			},
+			right: '10%' // 距离右边10%
+		},
+		grid: {
+			top: '20%',
+			left: '3%',
+			right: '4%',
+			bottom: '3%',
+			show: true, // 显示边框
+			borderColor: '#012f4a', // 边框颜色
+			containLabel: true
+		},
+		xAxis: {
+			type: 'category',
+			data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+			axisTick: {
+				show: false // 去除刻度线
+			},
+			axisLabel: {
+				color: '#4c9bfd' // 文本颜色
+			},
+			axisLine: {
+				show: false // 去除轴线
+			},
+			boundaryGap: false // 去除轴内间距
+		},
+		yAxis: {
+			type: 'value',
+			axisTick: {
+				show: false // 去除刻度
+			},
+			axisLabel: {
+				color: '#4c9bfd' // 文字颜色
+			},
+			splitLine: {
+				lineStyle: {
+					color: '#012f4a' // 分割线颜色
+				}
+			}
+		},
+		series: [{
+			name:'新增粉丝',
+      data:  [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+      type: 'line',
+      smooth: true
+
+			},
+			{
+				name:'新增游客',
+				data:[40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79],   
+				type: 'line',
+				smooth: true,
+			},
+
+		]
+	};
+
+	myChart.setOption(option);
+	window.addEventListener("resize", function () {
+		myChart.resize();
+	});
 })()
